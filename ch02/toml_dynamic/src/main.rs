ed1e39b4feff7b0667483641155495bd498cc01c
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-02-10 14:55:53 +0900

    ch02: toml and json

---
+++ ch02/toml_dynamic/src/main.rs
@@ -0,0 +1,21 @@
+use std::{env, fs};
+
+fn main() {
+    let config_const_values = {
+        let config_path = env::args().nth(1).unwrap();
+        let config_text = fs::read_to_string(&config_path).unwrap();
+        config_text.parse::<toml::Value>().unwrap()
+    };
+
+    println!("Original: {:#?}", config_const_values);
+    println!(
+        "[Postgresql].Database: {}",
+        config_const_values
+            .get("postgresql")
+            .unwrap()
+            .get("database")
+            .unwrap()
+            .as_str()
+            .unwrap()
+    );
+}
